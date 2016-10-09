0c3e1aaab3747e11fc5bab7f61e7b7f1eb9174e8
pazspm <pazspm@gmail.com>
2016-10-09 16:52:39 -0300

    Setting HeuristicCrawler

--- src/crawler/HeuristicCrawler.java
+++ src/crawler/HeuristicCrawler.java
@@ -1,35 +1,85 @@
 package crawler;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Scanner;
+import java.util.Set;
 
 import manager.Manager;
 
 public class HeuristicCrawler extends Crawler{
 
+	private ArrayList<String> bestLinks;
+	private ArrayList<String> otherLinks;
 	
+	private Random rand;
+	
+	private Set<String> words;
 	
 	public HeuristicCrawler(String base, String robotURL, Manager manager)
 			throws MalformedURLException, IOException {
 		super(base, robotURL, manager);
+		
+		rand = new Random();
+		
+		bestLinks = new ArrayList<String>();
+		otherLinks = new ArrayList<String>();
+		words = new HashSet<String>();
+		
+		File file = new File("heuristic_words");
+		Scanner in = new Scanner(file);
+		
+		while(in.hasNextLine()){
+			words.add(in.nextLine());
+		}
+		
+		
 	}
 
 	@Override
 	public void addLink(String link) {
-		// TODO Auto-generated method stub
+		boolean ok = false;
+		
+		String[] split = link.split("/");
+		for(int x=0;x<split.length;x++){
+			if(words.contains(split[x])){
+				ok = true;
+				break;
+			}
+		}
+		
+		if(ok){
+			bestLinks.add(link);
+		}else{
+			otherLinks.add(link);
+		}
 		
 	}
 
 	@Override
 	public int linksNumber() {
-		// TODO Auto-generated method stub
-		return 0;
+		return bestLinks.size() + otherLinks.size(); 
 	}
 
 	@Override
 	public String removeNextLink() {
-		// TODO Auto-generated method stub
-		return null;
+		int randNumber = rand.nextInt(100);
+		
+		if(bestLinks.size() > 0 && otherLinks.size() > 0){
+			if(randNumber <= 80){
+				return bestLinks.remove(0);
+			}else{
+				return otherLinks.remove(0);
+			}
+		}else if(bestLinks.size() > 0){
+			return bestLinks.remove(0);
+		}else{
+			return otherLinks.remove(0);
+		}
 	}
 
 }
--- src/crawler/Robot.java
+++ src/crawler/Robot.java
@@ -15,28 +15,35 @@ public class Robot {
 
 	private Set<String> disallowSet;
 
-	public Robot(String base, String robotURL) throws MalformedURLException, IOException{
+	public Robot(String base, String robotURL) {
 		disallowSet = new HashSet<String>();
 		
-		System.out.println(robotURL);		
-		InputStreamReader sr = new InputStreamReader(new URL(robotURL).openStream());		
-		BufferedReader bf = new BufferedReader(sr);
-		
-		String line;
-		String[] split;
-		while((line = bf.readLine()) != null){
-			split = line.split(" ");
-			if(split != null && split.length>=2){
-				if(split[0].equals("Disallow:")){
-					disallowSet.add(base + split[1].substring(1));
-					System.out.println(base + split[1].substring(1));
-				}
-			}
+		try {
+			InputStreamReader sr = new InputStreamReader(new URL(robotURL).openStream());
+			BufferedReader bf = new BufferedReader(sr);
 			
-			line = null;
-			split = null;
-		}
+			String line;
+			String[] split;
+			while((line = bf.readLine()) != null){
+				split = line.split(" ");
+				if(split != null && split.length>=2){
+					if(split[0].equals("Disallow:")){
+						disallowSet.add(base + split[1].substring(1));
+						System.out.println(base + split[1].substring(1));
+					}
+				}
+				
+				line = null;
+				split = null;
+			}
 		
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}		
 	}
 	
 	public boolean disallow(String url){
